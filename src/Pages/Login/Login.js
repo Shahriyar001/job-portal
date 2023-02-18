bdf26a2c791da0aaa9d48f15fd9345b015540385
Shahriyar001 <shahriyarmahbub55@gmail.com>
2023-02-18 08:10:00 +0600

    added firebase authentication

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import img from '../../assets/images/login/login.svg';
 
 const Login = () => {
@@ -13,28 +14,29 @@ const Login = () => {
                 <div className="text-center lg:text-left">
                     <img src={img} alt="" />
                 </div>
-                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100 py-20">
                     <h1 className="text-5xl text-center font-bold">Login</h1>
                     <form onSubmit={handleLogin} className="card-body">
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Email</span>
                             </label>
-                            <input type="text" placeholder="email" className="input input-bordered" />
+                            <input type="text" name='email' placeholder="email" className="input input-bordered" />
                         </div>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Password</span>
                             </label>
-                            <input type="text" placeholder="password" className="input input-bordered" />
+                            <input type="text" name='password' placeholder="password" className="input input-bordered" />
                             <label className="label">
-                                <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                                <a href="" className="label-text-alt link link-hover">Forgot password?</a>
                             </label>
                         </div>
                         <div className="form-control mt-6">
                             <input type="submit" value="Login" className="btn btn-primary" />
                         </div>
                     </form>
+                    <p className='text-center'>New to Job Portal? <Link className='text-orange-600 font-bold' to="/signup">Sign Up</Link></p>
                 </div>
             </div>
         </div>
